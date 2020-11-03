bbee919dae65c8b177f0597661a277bf9e71bb7e
MorganMBA <mba@intechinfo.fr>
2020-11-03 11:20:29 +0100

    instance of bdd

---
+++ config/dbb.php
@@ -0,0 +1,16 @@
+<?php
+    class db {
+        private $dbHost = 'localhost';
+        private $dbUser = 'root';
+        private $dbPass = '';
+        private $dbName = '';
+
+        //connect
+        public function connect() {
+            $mysql_connect_str = "mysql:host=$this->dbHost; dbname=$this->dbName";
+            $dbConnection = new PDO(mysql_connect_str, $this->dbUser, $this->dbPass);
+
+            $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $dbConnection;
+        }
+    }
